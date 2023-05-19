8b096775f5fecba5b6fcaa4f6a619d581aa68f65
Daniela Mabel Choquecondo Aspilcueta <mabelldaniela0105@gmail.com>
2023-05-18 20:38:11 -0500

    Segundo Commit Bienvenido al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,6 @@ public class HolaMundo {
 		System.out.printl("Hola Mundo");
 	}
 	//Segunda parte del commit "Bienvenido al curso"
-	System.out.println( "Bienvenido al curso de Pweb2 - Laboratorio")System.out.println( "Bienvenido al curso de Pweb2 - Laboratorio");
+	System.out.println( "Bienvenido al curso de Pweb2 - Laboratorio");
 }
 
