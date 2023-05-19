c28724973b83ab22f77effaf39e90c66698ad868
Daniela Mabel Choquecondo Aspilcueta <mabelldaniela0105@gmail.com>
2023-05-18 21:22:25 -0500

    Segundo Commit Bienvenido al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,14 +5,8 @@ public class HolaMundo {
 		System.out.printl("Hola Mundo");
 	}
 	//Segunda parte del commit "Bienvenido al curso"
-	Scanner scan = new Scanner (System.in);
+	System.out.printl("Bienvenido al curso");
+}
 
-	//Tercera parte del commit "Ingrese su nombre"
-	System.out.println("Ingrese su nombre");
-	String name = scan.next();
-
-	//Cuarta parte del commit "Imprima su nombre"
-	Syste.out.println("Bienvenido al curso " + name);
-} 
 
 
