2dabd6019c476166575f542215771a944154f989
Daniela Mabel Choquecondo Aspilcueta <mabelldaniela0105@gmail.com>
2023-05-18 21:28:35 -0500

    Segundo Commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,9 @@
 public class HolaMundo {
 	public static void main (String [] args {
-		System.out.printl("Hola Mundo");
+		System.out.println("Hola Mundo");
+
+		//Segunda parte del commit "Bienvenido al curso"
+		System.out.println("Bienvenido al curso");
 	}
 }
 
